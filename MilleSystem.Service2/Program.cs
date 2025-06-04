e4b721aa737ef07bafffc6aa4c62a59937658c6a
Wojciech Cz <wojciech.cz7@gmail.com>
2025-06-04 21:36:08 +0200

    Service 2 working OK without cache

---
+++ MilleSystem.Service2/Program.cs
@@ -0,0 +1,34 @@
+namespace MilleSystem.Service2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        
+        // Dodanie HttpClient jako us³ugi
+        builder.Services.AddHttpClient();
+        
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
